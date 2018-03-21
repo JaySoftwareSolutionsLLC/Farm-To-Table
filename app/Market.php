e8a4e18e54b65f8822ee9d847f242e64661704f9
JaySoftwareSolutionsLLC <Brettbrewster@jaysoftwaresolutions.com>
2018-03-20 20:06:02 -0400

    initial commit II

---
+++ app/Market.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Market extends Model
+{
+    public function farms() {
+        return $this->belongsToMany('App\Farm')->withTimestamps();
+    }
+    protected $fillable = ['name', 'city', 'website'];
+}
